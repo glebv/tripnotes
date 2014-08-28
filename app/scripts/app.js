864253ab2df7c15a072609ebcaa4206b6958d7b3
Gleb Vinnikov <gleb.vinnikov@gmail.com>
2014-08-28 11:26:50 +0300

    add yeoman angular scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tripnotesApp
+ * @description
+ * # tripnotesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tripnotesApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tripnotesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tripnotesApp
+ */
+angular.module('tripnotesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
